3e65dde25c7ff327f77d44199641ac0db5e75f6a
AreYouWorthy <wongben16@gmail.com>
2019-04-15 15:06:11 -0400

    createList not adding the first two characters WIP

--- Lab4/Lab4 source.cpp
+++ Lab4/Lab4 source.cpp
@@ -9,15 +9,22 @@ struct nodeType
 };
 
 void createList(nodeType*& first, nodeType*& last);
-
+void printList(nodeType*& first);
+void reversePrintList(nodeType*& last);
+//void insertNode(nodeType*& first, char letter);
 
 int main()
 {
 	nodeType* first,* last;
+	char letter;
 
 	createList(first, last);
 
-	
+	//cout << "Enter a letter to insert" << endl;
+	//cin >> letter;
+	//insertNode(first, letter);
+	printList(first);
+	reversePrintList(last);
 
 	system("PAUSE");
 	return 0;
@@ -26,7 +33,7 @@ int main()
 void createList(nodeType*& first, nodeType*& last) {
 
 	char letter;
-	nodeType* newNode,* current; 
+	nodeType* newNode, * current;
 
 	first = NULL;
 	last = NULL;
@@ -42,7 +49,7 @@ void createList(nodeType*& first, nodeType*& last) {
 	cin >> newNode->letter;
 	cout << endl;
 
-	while (letter != '0') {
+	while (letter != 'z') {
 		current = new nodeType;
 		current->prev = newNode;
 		current->next = NULL;
@@ -59,8 +66,66 @@ void createList(nodeType*& first, nodeType*& last) {
 
 		cout << "Enter a letter (Enter 0 to stop)." << endl;
 		cin >> newNode->letter;
+		letter = newNode->letter;
 		cout << endl;
 	}
+	/*char letter;
+	nodeType* newNode, * current;
+
+	first = NULL;
+	last = NULL;*/
 
 
+
+	///*cout << "Enter a letter (Enter 0 to stop)." << endl;
+	//cin >> letter;
+	//cout << endl;*/
+
+	//newNode = new nodeType;
+	//newNode->prev = NULL;
+	//newNode->next = NULL;
+	//cout << "Enter a letter (Enter ! to stop)." << endl;
+	//cin >> letter;
+	////newNode->letter = letter;
+	//cout << endl;
+
+	//while (letter != '!') {
+	//	
+	//	current = new nodeType;
+	//	current->prev = newNode;
+	//	current->next = NULL;
+	//	newNode = current;
+
+	//	if (first == NULL) {
+	//		first = newNode;
+	//		last = newNode;
+	//	}
+	//	else {
+	//		last->next = newNode;
+	//		last = newNode;
+	//	}
+
+	//	cout << "Enter a letter (Enter ! to stop)." << endl;
+	//	cin >> letter;
+	//	cout << endl;
+	//}
+}
+
+void printList(nodeType*& first) {
+	nodeType* current = new nodeType;
+	current = first;
+	while (current != NULL) {
+		cout << current->letter << endl;
+		current = current->next;
+	}
+}
+
+void reversePrintList(nodeType*& last) {
+	nodeType* current = new nodeType;
+	current = last;
+
+	while (current != NULL) {
+		cout << current->letter << endl;
+		current = current->prev;
+	}
 }
